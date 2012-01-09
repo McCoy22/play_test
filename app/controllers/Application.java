ce74ab831264a4f71f1cf61a48b854f71dab6a86
MoA <meyer.otte@gmail.com>
2012-01-09 14:51:52 +0100

    GUI

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,25 +1,40 @@
 package controllers;
 
+import java.util.List;
+
+import net.sf.cglib.transform.impl.AddDelegateTransformer;
+
+import models.Post;
+import play.Play;
 import play.data.validation.Required;
+import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(MenuInjector.class)
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+		render(frontPost, olderPosts);
+	}
+	
+	@Before
+	public static void addDefaults () {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
 
-    public static void test() {
-    	render();
-    }
-    
-    public static void sayHello (@Required String myname) {
-    	if (validation.hasErrors()) {
-    		flash.error("Fehler: Bitte Namen angeben!");
-    		index();
-    	}
-    	render(myname);
-    }
+	public static void test() {
+		render();
+	}
+
+	public static void sayHello(@Required String myname) {
+		if (validation.hasErrors()) {
+			flash.error("Fehler: Bitte Namen angeben!");
+			index();
+		}
+		render(myname);
+	}
 }
