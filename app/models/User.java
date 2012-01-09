1432e760c4156441122b1e5f2950eb2e9eafb350
MoA <meyer.otte@gmail.com>
2012-01-09 12:29:17 +0100

    Test

--- app/models/User.java
+++ app/models/User.java
@@ -10,6 +10,7 @@ public class User extends Model {
     public String email;
     public String password;
     public String fullname;
+    
     public boolean isAdmin;
     
     public User(String email, String password, String fullname) {
